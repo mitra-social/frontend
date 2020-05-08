4a88648d2d030125f2de55790c4dcac923802592
Franco Gervasi <fgervasi@objeng.ch>
2020-05-08 16:52:38 +0200

    Create mixin for article and note type

---
+++ src/views/home/post/text-type/ActivityStreamsArticleType.vue
@@ -0,0 +1,23 @@
+<template>
+  <TextType />
+</template>
+
+<script lang="ts">
+import { Component, Mixins } from "vue-property-decorator";
+
+import ActivityStreamsTextTypeMixin from "./ActivityStreamsTextTypeMixin";
+
+@Component
+export default class ActivityStreamsArticleType extends Mixins(
+  ActivityStreamsTextTypeMixin
+) {}
+</script>
+<style lang="scss" scoped>
+.post {
+  margin: 5px;
+}
+
+.v-card__text {
+  width: inherit;
+}
+</style>
