58e4ece4771ef9eb127c7ffdbf763471eb41d6bb
Franco Gervasi <fgervasi12@gmail.ch>
2020-06-24 18:06:12 +0200

    Remove unused console info

--- src/router/index.ts
+++ src/router/index.ts
@@ -40,8 +40,6 @@ router.beforeEach((to, from, next) => {
   const publicPages = ["/login", "/signup"];
   const authRequired = !publicPages.includes(to.path);
 
-  console.info(authRequired);
-  console.info(!store.getters["Auth/isAuthenticated"]);
   if (authRequired && !store.getters["Auth/isAuthenticated"]) {
     next({ name: "login" });
   } else if (authRequired && !store.getters["User/isUserFetch"]) {
