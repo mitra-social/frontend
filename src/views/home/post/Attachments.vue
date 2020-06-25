48b80a3e272f5ac97772b8854dfa8d719c22d75f
TiMESPLiNTER <github@timesplinter.ch>
2020-06-25 07:41:58 +0200

    feat(): wip

--- src/views/home/post/Attachments.vue
+++ src/views/home/post/Attachments.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="attachments.length > 0">
-    <div v-for="(attachment, index) in attachments" :key="index">
+  <div v-if="getAttachments.length > 0">
+    <div v-for="(attachment, index) in getAttachments" :key="index">
       <a :href="attachment.url">{{ attachment.url }}</a>
     </div>
   </div>
@@ -23,32 +23,32 @@ export default class ActivityStreamsAttachments extends Vue {
       ? this.attachments
       : [this.attachments];
 
-    const foo: Array<Attachment | undefined> = normalizedAttachments.map<Attachment | undefined>(
-      (param: ActivityObject | URL, index: number): Attachment | undefined => {
-        if ((param as ActivityObject).type) {
-          const object = param as ActivityObject;
-          const objectUrl: Link | URL | undefined = !Array.isArray(object.url)
-            ? object.url
-            : object.url[0];
+    const foo: Array<Attachment | undefined> = normalizedAttachments.map<
+      Attachment | undefined
+    >((param: ActivityObject | URL, index: number): Attachment | undefined => {
+      if ((param as ActivityObject).type) {
+        const object = param as ActivityObject;
+        const objectUrl: Link | URL | undefined = !Array.isArray(object.url)
+          ? object.url
+          : object.url[0];
 
-          if (objectUrl === undefined) {
-            return undefined;
-          }
-
-          const url: URL = (objectUrl as Link).type
-            ? (objectUrl as Link).href
-            : (objectUrl as URL);
-
-          return new Attachment(url.href, object.mediaType);
+        if (objectUrl === undefined) {
+          return undefined;
         }
 
-        return new Attachment((param as URL).href, undefined);
-      }
-    );
+        const url: URL = (objectUrl as Link).type
+          ? (objectUrl as Link).href
+          : (objectUrl as URL);
 
-    return foo.filter<Attachment>(
-      (item: Attachment | undefined) => item !== undefined
-    );
+        return new Attachment(url.href, object.mediaType);
+      }
+
+      return new Attachment((param as URL).href, undefined);
+    });
+
+    return foo
+      .filter((item) => item !== undefined)
+      .map((item) => item as Attachment);
   }
 }
 </script>
