ef867116757b06f4903417330feefa96b5cb99da
Franco Gervasi <fgervasi12@gmail.ch>
2020-06-28 16:01:11 +0200

    Add navigation drawer

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,50 @@
 <template>
-  <div class="home-content" no-gutters>
-    <div class="following-actors">
+  <div class="content" no-gutters>
+    <div class="following-actors" v-if="isFollowingLoading">
       <FollowingActors />
     </div>
-    <div class="posts">
+    <div class="posts" v-if="isFollowingLoading">
       <Posts />
     </div>
+
+    <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent>
+      <v-list-item class="px-2">
+        <v-avatar color="indigo" v-if="getUser.icon">
+          <v-img :src="getUser.icon"></v-img>
+        </v-avatar>
+        <v-avatar color="indigo" size="36" v-else>
+          <v-icon dark>mdi-account-circle</v-icon>
+        </v-avatar>
+        <v-list-item-content>
+          <v-list-item-title>{{ getUser.preferredUsername }}</v-list-item-title>
+          <v-list-item-subtitle v-if="getUser.email">
+            email{{ getUser.email }}</v-list-item-subtitle
+          >
+        </v-list-item-content>
+        <v-btn icon @click.stop="mini = !mini">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon v-if="$vuetify.theme.dark">mdi-moon-waning-crescent</v-icon>
+            <v-icon v-else>mdi-white-balance-sunny</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-action>
+            <v-switch
+              v-model="$vuetify.theme.dark"
+              hide-details
+              inset
+            ></v-switch>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 
@@ -18,6 +57,7 @@ import Posts from "./post/Posts.vue";
 import { User } from "@/model/user";
 
 const userStore = namespace("User");
+const followingStore = namespace("Following");
 
 @Component({
   components: {
@@ -26,8 +66,23 @@ const userStore = namespace("User");
   },
 })
 export default class MitraHome extends Vue {
+  private isFollowingLoading = false;
+  private drawer = true;
+  private mini = true;
+
   @userStore.Getter
   public getUser!: User;
+
+  @followingStore.Action
+  public fetchFollowing!: (user: string) => Promise<void>;
+
+  private created() {
+    if (this.getUser) {
+      this.fetchFollowing(this.getUser.preferredUsername).then(() => {
+        this.isFollowingLoading = true;
+      });
+    }
+  }
 }
 </script>
 
@@ -35,23 +90,11 @@ export default class MitraHome extends Vue {
 body {
   background-color: #333;
 }
-.home-content {
+.content {
+  width: 100%;
   height: 100%;
-  -webkit-box-flex: 1;
-  -ms-flex: 1 1 auto;
-  flex: 1 1 auto;
-  min-height: 0px;
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -ms-flex-wrap: wrap;
-  flex-wrap: wrap;
-}
-
-.following-actors,
-.posts {
-  height: 100%;
-  overflow: auto;
+  flex-direction: row;
 }
 
 .following-actors {
@@ -63,5 +106,6 @@ body {
   flex-basis: 0;
   flex-grow: 1;
   width: 100%;
+  min-height: 100%;
 }
 </style>
