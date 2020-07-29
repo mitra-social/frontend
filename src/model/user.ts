7cd62f5f6e5870b06257d66cabb579d7fa74a4da
Franco Gervasi <fgervasi12@gmail.ch>
2020-07-29 14:56:27 +0200

    Fix user prop preferredUsername

--- src/model/user.ts
+++ src/model/user.ts
@@ -3,6 +3,7 @@ import { ActorType, Actor } from "activitypub-objects";
 export interface User extends Actor {
   type: ActorType.PERSON;
   userId: string;
+  preferredUsername: string;
   email: string;
   registeredAt: Date;
   inbox: string;
