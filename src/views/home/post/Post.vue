095e59c628c1e6889f2cc7c666795c4ca19c91aa
Franco Gervasi <fgervasi12@gmail.ch>
2020-08-13 15:43:29 +0200

    In repley to open with expansion

--- src/views/home/post/Post.vue
+++ src/views/home/post/Post.vue
@@ -1,54 +1,75 @@
 <template>
-  <v-card class="post" v-if="post">
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title class="headline" v-if="post.name || post.summary">{{
-          post.name ? post.name : post.summary | stripHtmlTags
-        }}</v-list-item-title>
-        <v-list-item-subtitle>
-          <div class="d-flex flex-row justify-space-between">
-            <ActorPin v-if="post.attributedTo" :actor="post.attributedTo" />
-            <Date
-              v-if="post.published"
-              icon="mdi-publish"
-              :date="post.published"
-            />
-            <Date v-if="post.updated" icon="mdi-update" :date="post.updated" />
-          </div>
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-    <v-divider class="mx-4"></v-divider>
-    <v-card-text>
-      <component :is="getComponent(post.type)" :data="post" />
-      <Attachments
-        v-if="
-          post.attachment &&
-          ((Array.isArray(post.attachment) && post.attachment.length > 0) ||
-            !Array.isArray(post.attachment))
-        "
-        :attachments="post.attachment"
-      />
-    </v-card-text>
-    <v-divider class="mx-4"></v-divider>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        color="blue lighten-1"
-        :disabled="!post.inReplyTo"
-        @click="isCommentOpen = !isCommentOpen"
+  <div>
+    <v-card class="post" v-if="post">
+      <v-expansion-panels
+        class="in-reply-to-expansion"
+        flat
+        v-if="!!post.inReplyTo && typeof post.inReplyTo === 'object'"
       >
-        <v-icon>mdi-comment-outline</v-icon>
-      </v-btn>
-      <v-btn icon disabled>
-        <v-icon>mdi-heart-circle-outline</v-icon>
-      </v-btn>
-    </v-card-actions>
-    <div class="pa-1" v-show="isCommentOpen">
-      <Post :post="post.inReplyTo" />
-    </div>
-  </v-card>
+        <v-expansion-panel>
+          <v-expansion-panel-header
+            color="grey lighten-4 grey--text text--darken-1"
+            >View post that would be replied to</v-expansion-panel-header
+          >
+          <v-expansion-panel-content
+            class="in-reply-to-expansion-content"
+            color="grey lighten-4"
+          >
+            <Post class="in-reply-to" :post="post.inReplyTo" />
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title
+            class="headline"
+            v-if="post.name || post.summary"
+            >{{
+              post.name ? post.name : post.summary | stripHtmlTags
+            }}</v-list-item-title
+          >
+          <v-list-item-subtitle>
+            <div class="d-flex flex-row justify-space-between">
+              <ActorPin v-if="post.attributedTo" :actor="post.attributedTo" />
+              <Date
+                v-if="post.published"
+                icon="mdi-publish"
+                :date="post.published"
+              />
+              <Date
+                v-if="post.updated"
+                icon="mdi-update"
+                :date="post.updated"
+              />
+            </div>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider class="mx-4"></v-divider>
+      <v-card-text>
+        <component :is="getComponent(post.type)" :data="post" />
+        <Attachments
+          v-if="
+            post.attachment &&
+            ((Array.isArray(post.attachment) && post.attachment.length > 0) ||
+              !Array.isArray(post.attachment))
+          "
+          :attachments="post.attachment"
+        />
+      </v-card-text>
+      <v-divider class="mx-4"></v-divider>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn icon disabled>
+          <v-icon>mdi-comment-outline</v-icon>
+        </v-btn>
+        <v-btn icon disabled>
+          <v-icon>mdi-heart-circle-outline</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts">
@@ -86,7 +107,6 @@ export default class Post extends Vue {
   }
 }
 </script>
-
 <style lang="scss" scoped>
 .post {
   margin: 5px;
@@ -99,5 +119,14 @@ export default class Post extends Vue {
 
 .v-card__text {
   width: inherit;
+}
+
+.in-reply-to > .theme--light.v-card {
+  background-color: #e0e0e0;
+}
+
+.in-reply-to-expansion > .v-expansion-panel--active > .v-expansion-panel-header,
+.in-reply-to-expansion > .v-expansion-panel-header {
+  min-height: 30px;
 }
 </style>
