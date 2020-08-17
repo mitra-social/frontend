f92563283e91456b42d6f5d7f84422bd7c9a12a0
Franco Gervasi <fgervasi12@gmail.ch>
2020-08-17 19:10:02 +0200

    Uncomment login success test

--- tests/unit/views/Login.spec.ts
+++ tests/unit/views/Login.spec.ts
@@ -18,7 +18,7 @@ describe("Login.vue", () => {
   // eslint-disable-next-line
   let vuetify: any;
   const username = "john.doe";
-  // const password = "123";
+  const password = "123";
   // eslint-disable-next-line
   const user = (userData as any) as User
 
@@ -38,19 +38,19 @@ describe("Login.vue", () => {
     AuthenticationUtil.clear();
   });
 
-  // it("Login success", async () => {
-  //   const wrapper = mount(Login, { localVue, vuetify, router, store });
+  it("Login success", async () => {
+    const wrapper = mount(Login, { localVue, vuetify, router, store });
 
-  //   wrapper.find('input[name="login"]').setValue(username);
-  //   wrapper.find('input[name="password"]').setValue(password);
-  //   await flushPromises();
+    wrapper.find('input[name="login"]').setValue(username);
+    wrapper.find('input[name="password"]').setValue(password);
+    await flushPromises();
 
-  //   wrapper.find("form").trigger("submit.prevent");
-  //   await flushPromises();
+    wrapper.find("form").trigger("submit.prevent");
+    await flushPromises();
 
-  //   expect(wrapper.find(".v-alert").exists()).toBe(false);
-  //   expect(router.currentRoute.path).toBe("/");
-  // });
+    expect(wrapper.find(".v-alert").exists()).toBe(false);
+    expect(router.currentRoute.path).toBe("/");
+  });
 
   // it("Login wrong user", async () => {
   //   const wrapper = mount(Login, { localVue, vuetify, router, store });
